bcd11804771dde7a9a7650e22410f6864c18bca2
Enias Oliveira <eniasoliveira27@gmail.com>
2022-06-18 21:40:30 -0300

    feat(wip): adds base of header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import TodoLogo from './assets/todo-logo.svg'
+
 import './styles/global.css'
+import styles from './App.module.css'
 
 function App() {
-  return (
-    <div className="App">
-      Hello There
-    </div>
-  )
+    return (
+        <>
+            <header className={styles.header}>
+                <img src={TodoLogo} alt="Todo Logo" />
+            </header>
+        </>
+    )
 }
 
 export default App
