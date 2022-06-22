9fc7ce8fb2a65fe9e5c0e05a6a0160c14e0cc1f0
Enias Oliveira <eniasoliveira27@gmail.com>
2022-06-21 22:12:32 -0300

    feat(wip): adds task creation and check functionality

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,43 @@ import ClipboardIcon from './assets/clipboard-icon.svg'
 
 import './styles/global.css'
 import styles from './App.module.css'
-import { useState } from 'react'
+import { FormEvent, useState } from 'react'
 
 export const App = () => {
-  const [checked, setChecked] = useState(false)
+  const [tasks, setTasks] = useState([
+    {
+      completed: false,
+      description: 'Hello there',
+    },
+    {
+      completed: true,
+      description:
+        'Nunc eget lorem dolor, sed? Sagittis vitae et leo duis diam quam nulla porttitor massa id neque',
+    },
+  ])
+
+  const [newTaskDescription, setNewTaskDescription] = useState('')
 
   return (
     <>
       <Header />
       <div className={styles.wrapper}>
-        <form className={styles.newTaskWrapper}>
+        <form
+          className={styles.newTaskWrapper}
+          onSubmit={(e: FormEvent) => {
+            e.preventDefault()
+            setTasks((prevTasks) => [
+              ...prevTasks,
+              { completed: false, description: newTaskDescription },
+            ])
+            setNewTaskDescription('')
+          }}
+        >
           <input
             type='text'
             name='new-task'
-            value=''
+            value={newTaskDescription}
+            onChange={(e) => setNewTaskDescription(e.target.value)}
             placeholder='Adicione uma nova tarefa'
           />
           <button type='submit' name='add-new-task'>
@@ -36,34 +59,54 @@ export const App = () => {
             </strong>
           </header>
           <main>
-            {false ? (
+            {tasks.length === 0 ? (
               <div className={styles.emptyTasks}>
                 <img src={ClipboardIcon} alt='clipboard icon' />
               </div>
             ) : (
               <ul className={styles.tasksList}>
-                <li>
-                  <label>
-                    <input
-                      type='checkbox'
-                      onChange={() => setChecked((state) => !state)}
-                    />
-                    <span
-                      className={
-                        checked ? styles.checkboxActive : styles.checkbox
-                      }
-                    >
-                      {checked && <Check size={10} weight='bold' />}
-                    </span>
-                    <p>
-                      Nunc eget lorem dolor, sed? Sagittis vitae et leo duis ut
-                      diam quam nulla porttitor massa id neque aliquam
-                    </p>
-                  </label>
-                  <button>
-                    <Trash size={16} />
-                  </button>
-                </li>
+                {tasks.map((task) => (
+                  <li>
+                    <label>
+                      <input
+                        type='checkbox'
+                        onChange={() =>
+                          setTasks((prevTasks) =>
+                            prevTasks.map((prevTask) =>
+                              prevTask.description === task.description
+                                ? {
+                                    ...prevTask,
+                                    completed: !prevTask.completed,
+                                  }
+                                : prevTask,
+                            ),
+                          )
+                        }
+                      />
+                      <span
+                        className={
+                          task.completed
+                            ? styles.checkboxActive
+                            : styles.checkbox
+                        }
+                      >
+                        {task.completed && <Check size={10} weight='bold' />}
+                      </span>
+                      <p
+                        className={
+                          task.completed
+                            ? styles.checkedTask
+                            : styles.defaultTask
+                        }
+                      >
+                        {task.description}
+                      </p>
+                    </label>
+                    <button>
+                      <Trash size={16} />
+                    </button>
+                  </li>
+                ))}
               </ul>
             )}
           </main>
